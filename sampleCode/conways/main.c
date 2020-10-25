df152ec3b34f2a6d9ec4a3c8a37283d839a5f900
antonio97Tu@gmail.com <antonio97Tu@gmail.com>
2020-10-25 19:45:17 +0100

    Tests using ncourses lib

--- sampleCode/conways/main.c
+++ sampleCode/conways/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ncurses.h>
+#include <stdlib.h>
 
 #include "conway.h"
 
@@ -9,6 +10,8 @@ int main()
   printw("Hello World !!!");	/* Print Hello World		  */
   refresh();			/* Print it on to the real screen */
   mvaddch(10,10,'X');
+  mvaddch(0,0,'X');
+  
     refresh();			/* Print it on to the real screen */
 
   getch();			/* Wait for user input */
